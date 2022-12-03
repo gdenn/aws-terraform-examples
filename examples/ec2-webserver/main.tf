5fe3e37f6cd6badd07962e971612db89725848ac
Dennis Groß <gdennis91@googlemail.com>
2022-12-03 17:39:07 +0100

    Add alb to web-server ec2 instance

--- examples/ec2-webserver/main.tf
+++ examples/ec2-webserver/main.tf
@@ -30,6 +30,8 @@ resource "aws_instance" "web_server" {
     aws_iam_role_policy_attachment.cw_agent_policy_attachment
   ]
 
+  security_groups = [ aws_security_group.web_server_sg.id ]
+
   root_block_device {
     delete_on_termination = true
     volume_type = "gp3"
@@ -115,5 +117,127 @@ data "aws_ami" "amzn2" {
 }
 
 #####################################################
+# WEB SERVER SECURITY GROUP
+#####################################################
+
+resource "aws_security_group" "web_server_sg" {
+  name = "web-server-sg"
+  description = "allow http traffic from alb"
+  vpc_id = module.vpc.vpc_id
+  ingress = [ 
+    {
+      description = "allow http from alb"
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      security_groups = [aws_security_group.alb_security_group.id]
+      cidr_blocks = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      self             = false
+    }
+  ]
+}
+
+#####################################################
+# ALB SECURITY GROUP
+#####################################################
+
+resource "aws_security_group" "alb_security_group" {
+  name = "alb-security-group"
+  description = "alb security group"
+  vpc_id = module.vpc.vpc_id
+  ingress = [ 
+    {
+      description = "allow https traffic from the internet"
+      from_port = 443
+      to_port = 443
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+  egress = [ 
+    {
+      description = "allow http traffic to web server ec2 instance"
+      from_port = 443
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+}
+
+#####################################################
+# ALB TARGET GROUP
+#####################################################
+
+resource "aws_lb_target_group" "web_server_target_group" {
+  name = "web-server-target-group"
+  port = 80
+  target_type = "instance"
+  protocol = "HTTP"
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_alb_target_group_attachment" "web_server_target_group_attachement" {
+  count = length(aws_instance.web_server)
+  target_group_arn = aws_lb_target_group.web_server_target_group.arn
+  target_id = element(aws_instance.web_server.*.id, count.index)
+}
+
+#####################################################
 # ALB
 #####################################################
+resource "aws_alb" "alb" {
+  name = "alb"
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.alb_security_group.id]
+  subnets = module.vpc.web_subnet
+}
+
+#####################################################
+# ALB LISTENER
+#####################################################
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_alb.alb.arn
+  port = "80"
+  protocol = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port = "443"
+      protocol = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+#####################################################
+# ALB LISTENER RULES
+#####################################################
+resource "aws_alb_listener_rule" "static_rule" {
+  listener_arn = aws_lb_listener.alb_listener.arn
+  priority = 100
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.web_server_target_group.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/var/www/html/index.html"]
+    }
+  }
+}
