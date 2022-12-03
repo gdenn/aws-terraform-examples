ed78081588c026abc97ced6eee0801c6f64346f6
Dennis Groß <gdennis91@googlemail.com>
2022-12-03 10:17:59 +0100

    Fix ssm agent user data script

--- examples/ec2-webserver/user-data.sh
+++ examples/ec2-webserver/user-data.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+
+#################### AWS SESSION MANAGER AGENT ####################
+sudo mkdir /tmp/ssm
+cd /tmp/ssm
+wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
+
+sudo dpkg -i amazon-ssm-agent.deb
+sudo systemctl enable amazon-ssm-agent
+rm amazon-ssm-agent.deb
