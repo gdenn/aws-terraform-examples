e8eb3840765cf1eff9b2ed79ea92f32e1af00ac5
Dennis Groß <gdennis91@googlemail.com>
2022-12-03 10:28:16 +0100

    Setup apache http

--- examples/ec2-webserver/user-data.sh
+++ examples/ec2-webserver/user-data.sh
@@ -8,3 +8,12 @@ wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64
 sudo dpkg -i amazon-ssm-agent.deb
 sudo systemctl enable amazon-ssm-agent
 rm amazon-ssm-agent.deb
+
+#################### APACHE2 WEB SERVER ####################
+sudo yum update -y
+sudo amazon-linux-extras install php8.0 mariadb10.5 -y
+sudo yum install -y httpd
+
+user=$(whoami)
+sudo chown $user:$user /var/www/html/index.html
+echo “Hello World from $(hostname -f)” > /var/www/html/index.html
