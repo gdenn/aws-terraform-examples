74157cc082ebb2c4167bec6391e391d7af9b27a2
Dennis Groß <gdennis91@googlemail.com>
2022-12-17 20:56:37 +0100

    Use forward listener action

--- examples/ecs-cluster/main.tf
+++ examples/ecs-cluster/main.tf
@@ -135,17 +135,11 @@ resource "aws_alb" "ecs_alb" {
 resource "aws_alb_listener" "http_listener" {
   load_balancer_arn = aws_alb.ecs_alb.arn
   port = 80
-  protocol = "tcp"
+  protocol = "HTTP"
 
   default_action {
-    type = "redirect"
+    type = "forward"
     target_group_arn = aws_alb_target_group.service_target_group.arn
-  
-    redirect {
-      port = local.host_port
-      protocol = "HTTP"
-      status_code = "HTTP_301"
-    }
   }
 }
 
